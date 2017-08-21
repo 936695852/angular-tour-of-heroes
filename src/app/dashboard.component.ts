2235dabf4346807f78d33e81eb747307c351c613
Lenin <wke1018@gamil.com>
2017-08-21 21:07:06 +0800

    finish

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -6,8 +6,9 @@ import { HeroService } from './hero.service';
 @Component({
   selector: 'my-dashboard',
   templateUrl: './dashboard.component.html',
+  styleUrls: [ './dashboard.component.css' ]
 })
-export class DashboardComponent implements OnInit{ 
+export class DashboardComponent implements OnInit{
   heroes: Hero[] = [];
 
   constructor(private heroService: HeroService) { }
